daac8746b2e2adae10c7e85ae66274fa06e98e26
maiko.trindade <maikocharles@gmail.com>
2023-05-03 08:49:31 -0600

    Added config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+const privateKey = process.env.PRIVATE_KEY;
+const etherscanKey = process.env.ETHERSCAN_KEY;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}}`,
+      accounts: [`0x${privateKey!}`]
+    }
+  },
   solidity: {
     version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
+        runs: 200
       },
     },
   },
+  etherscan: {
+    apiKey: etherscanKey
+  }
 };
 
 export default config;
